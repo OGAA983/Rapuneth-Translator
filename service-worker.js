7ed260e0f6c8c94aaa1bd6189e7752719e022567
OGAA983 <Ogamnv1@gmail.com>
2025-06-28 20:35:25 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,39 @@
-const CACHE_NAME = 'rapuneth-translator-v1';
-const URLS_TO_CACHE = [
-  '/rapuneth-translator/',
-  '/rapuneth-translator/index.html',
-  '/rapuneth-translator/manifest.json',
-  '/rapuneth-translator/icons/icon-192.png',
-  '/rapuneth-translator/icons/icon-512.png'
-  // Add other assets: CSS, JS files if needed
+const CACHE_NAME = "rapuneth-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+  // You can add other assets like CSS, JS files here if you want
 ];
 
-// Install the service worker and cache all necessary files
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Activate the service worker and clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch files from cache, fallback to network
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
   );
 });
